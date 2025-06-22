c3965960dcec07e06512ca5cc1fb675b68578d1e
Hanashiko <hlichisper@gmail.com>
2025-06-22 15:31:25 +0300

    feat: added support for second monitor

--- launch.sh
+++ launch.sh
@@ -6,5 +6,14 @@ killall -q polybar
 # Очікувати завершення процесів
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+MONITORS=($(polybar --list-monitors | cut -d: -f1))
+
+if [[ ${#MONITORS[@]} -ge 2 ]]; then
+    MONITOR="${MONITORS[0]}" polybar bar1 &
+    MONITOR="${MONITORS[1]}" polybar bar2 &
+else
+    MONITOR="${MONITORS[0]}" polybar bar1 &
+fi
+
 # Запуск polybar з конфігом
-polybar bar1 &
+# polybar bar1 &
